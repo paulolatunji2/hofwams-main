165363065283915fd8ac5cd5b324eed7797ab277
mujeebahmad <abdulmujeebahmad03@gmail.com>
2024-09-05 13:34:10 +0100

    update the loading ui

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import { LeafIcon } from "@/components/gen/icons";
 const Loading = () => {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
-      <div className="animate-pulse duration-300 text-[40px] flex gap-2 items-center">
+      <div className="animate-pulse text-[40px] flex gap-2 items-center">
         <LeafIcon className="size-10" />
         Hofwams
       </div>
